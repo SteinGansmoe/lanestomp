63e3acfd9a50cee71e7882f85216cde9a6db5e50
SteinGansmoe <steina89@hotmail.com>
2026-05-14 20:17:37 +0200

    Initital Next.js setup with Tailwind and shadcn/ui

---
+++ src/data/games.ts
@@ -0,0 +1,46 @@
+export const games = [
+  {
+    id: "diablo-4",
+    name: "Diablo IV",
+    genre: "ARPG",
+    image: "...",
+    season: {
+      title: "Season 13",
+      startDate: "2026-06-01",
+      endDate: "2026-08-15",
+    },
+  },
+  {
+    id: "last-epoch",
+    name: "Last Epoch",
+    genre: "ARPG",
+    image: "...",
+    season: {
+      title: "Season 4",
+      startDate: "2026-06-01",
+      endDate: "2026-08-15",
+    },
+  },
+  {
+    id: "league-of-legends",
+    name: "League of Legends",
+    genre: "MOBA",
+    image: "...",
+    season: {
+      title: "Season 16",
+      startDate: "2026-06-01",
+      endDate: "2026-08-15",
+    },
+  },
+  {
+    id: "path-of-exile",
+    name: "Path of Exile",
+    genre: "ARPG",
+    image: "...",
+    season: {
+      title: "Season 21",
+      startDate: "2026-06-01",
+      endDate: "2026-08-15",
+    },
+  },
+];
